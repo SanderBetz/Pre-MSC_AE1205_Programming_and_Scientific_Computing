bb7ef0fc9c079506e3fe69c71a7892c99e5291ea
SanderBetz <sander.betz@me.com>
2024-04-27 12:38:38 +0200

    Added personal info

--- Assignment 2/Assignment 2.py
+++ Assignment 2/Assignment 2.py
@@ -4,34 +4,39 @@
 # Put imports here
 import random
 
-max_guesses = 10
-guess_num = 0
+max_guesses : int = 10
+guess_num   : int = 0
 
 # Main code
-def init_game():
-    def generate_sequence():
-        # This function may look weird, however it is recursion safe
-        # The function checks if all numbers are unique, if it is detected that there are more instances of a number in
-        # the sequence, the function calls itself again, and triggers the BREAK. This ensures that when a new sequence
-        # is generated, the for loop will not check other numbers in the original sequence. Therefore, if multiple
-        # sequences are flawed and one is generated good. The function will collapse due to a chain of breaks in subsequent functions.
-        seq = random.sample([i for i in range(1, 7)], 4)
-        for i in range(6):
-            if seq.count(i) > 1:
-                seq = generate_sequence()
-                break
-        return seq
+def generate_sequence() -> list:
+    # This function may look weird, however it is recursion safe
+    # The function checks if all numbers are unique, if it is detected that there are more instances of a number in
+    # the sequence, the function calls itself again, and triggers the BREAK. This ensures that when a new sequence
+    # is generated, the for loop will not check other numbers in the original sequence. Therefore, if multiple
+    # sequences are flawed and one is generated good. The function will collapse due to a chain of breaks in subsequent functions.
+    seq = random.sample([i for i in range(1, 7)], 4)
+    for i in range(6):
+        if seq.count(i) > 1:
+            seq = generate_sequence()
+            break
 
+    print(seq)
+    return seq
+
+def init_game() -> list:
+    global game_sequence
     game_sequence = generate_sequence()
-
     print(" *** Mastermind Game! *** ")
 
     return game_sequence
 
-def ask_guess(cur_guess : int) -> list[int]:
+def ask_guess(cur_guess: int) -> list[int]:
     seq = input(f'Which 4 number sequence do you want to input? \n'
                 f'You have {max_guesses - cur_guess} attempts left\n'
                 f'Sequence: ')
+    if len(seq) != 4:
+        print(f'--> You entered {len(seq)} numbers, please input 4 numbers for the game!')
+        seq = ask_guess(cur_guess)
     for char in seq:
         if int(char) <= 0 or int(char) > 6:
             print('--> One or more numbers are not valid')
@@ -40,8 +45,8 @@ def ask_guess(cur_guess : int) -> list[int]:
 
     return guess
 
-def compare_guess(game_sequence, guess_sequence):
-    guess_states = [0, 0, 0, 0]
+def compare_guess(game_sequence: list, guess_sequence: list) -> list:
+    guess_states : list= [0, 0, 0, 0]
     for n, key, value in zip([0, 1, 2, 3], guess_sequence, game_sequence):
         if key == value:
             guess_states[n] = 2
@@ -58,11 +63,14 @@ def evaluate_guess(states: list) -> None:
 
 def restart_game() -> bool:
     global guess_num
-    possibilities = ['', 'y', 'yes']
+    global game_sequence
+    possibilities : list = ['', 'y', 'yes']
     ans = input("Do you want to play another game? Press ENTER, Y, or type YES\n"
                 "You entered: ").lower()
     if ans in possibilities:
+        game_sequence = generate_sequence()
         guess_num = 0
+
         return True
     print("--> You chose not to replay. Thank you for playing! Goodbye!")
     return False
@@ -82,14 +90,14 @@ def end_game_handling(curr_guess: int, states: list, win_sequence: list) -> bool
 
 
 def main():
-    debug_mode = True
-    not_guessed = True
+    not_guessed : bool = True
     global guess_num
+    global game_sequence
 
+    # Initialize the game
     game_sequence = init_game()
 
     while not_guessed:
-        print(game_sequence)
         guess = ask_guess(guess_num)
         states = compare_guess(game_sequence, guess)
         evaluate_guess(states)
